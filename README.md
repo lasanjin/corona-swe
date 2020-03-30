45bb478e0a477995eb26a3bdee526acc89514639
lasanjin <sanjinslavnic@gmail.com>
2020-03-30 21:23:28 +0200

    Ads scraper & hax for collecting Corona data from Folkhälsomyndigheterna

---
+++ README.md
@@ -0,0 +1,21 @@
+# Corona statistics for Sweden
+Data from [Folkhälsomydigheten](https://www.folkhalsomyndigheten.se/smittskydd-beredskap/utbrott/aktuella-utbrott/covid-19/aktuellt-epidemiologiskt-lage/). Runs with Python 3+
+
+
+## How to run
+ - [Scraper](fhm-scraper.py) 
+   - **NOTE**
+   - If no Chrome-browser
+       1. Download from [source](https://raw.githubusercontent.com/Bugazelle/chromium-all-old-stable-versions/master/chromium.stable.json) or for [Linux](https://www.googleapis.com/download/storage/v1/b/chromium-browser-snapshots/o/Linux_x64%2F722276%2Fchrome-linux.zip?generation=1575588380806233&alt=media) (version <= 80)
+       2. Unpack to `driver/` and rename to `chromium` 
+```
+$ ./fhm-scraper.py
+```
+
+</br>
+
+ - [Hax](fhm-hax.py)
+   - Much faster alternative (gets data directly from API)
+```
+$ ./fhm-hax.py
+```
---
+++ fhm-scraper.py
@@ -0,0 +1,163 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+import json
+import time
+from datetime import datetime, date
+from collections import OrderedDict, Counter
+
+from modules.bs4 import BeautifulSoup
+from modules.sortedcontainers import SortedSet, SortedDict
+
+from modules.selenium import webdriver
+from modules.selenium.webdriver.common.by import By
+from modules.selenium.webdriver.support.ui import WebDriverWait
+from modules.selenium.webdriver.support import expected_conditions as ec
+from modules.selenium.common.exceptions import TimeoutException
+
+
+def main():
+    print(C.FDATA)
+
+    driver = build_driver()
+    driver.get(api.API)
+
+    npage = get_element(driver, C.NDAYS_ID).text
+    button = get_element(driver, C.NEXT_ID)
+    ndays = int(npage[npage.rindex(' ')+1:])
+
+    time_series = parse_data(driver, button, ndays)
+    data = parse_time_series(time_series)
+
+    # save_data(data)
+
+    print_json(data)
+
+
+def parse_data(driver, button, ndays):
+    time_series = SortedDict()
+
+    for i in range(ndays):
+        soup = BeautifulSoup(driver.page_source, 'html.parser')
+        div = soup.find(id=C.TABLE_ID)
+        tds = div.find_all('td')
+
+        dt = tds[1].text
+        date = format_date(dt)
+        time_series[date] = OrderedDict()
+        region = ''
+
+        for i, td in enumerate(tds[2:]):
+
+            if i % 2 != 0:
+                n = int(td.text)
+                time_series[date][region] = n
+
+            region = td.text
+
+        button.click()
+
+    driver.quit()
+
+    return time_series
+
+
+def parse_time_series(time_series):
+    data = dict()
+    data['time_series'] = time_series
+    data['total'] = sum_time_series(time_series)
+
+    return data
+
+
+def sum_time_series(time_series):
+    return sum(
+        (Counter(dict(x)) for x in list(time_series.values())),
+        Counter())
+
+
+def build_driver():
+    # prepare the option for the chrome driver
+    options = webdriver.ChromeOptions()
+    options.add_argument('--headless')
+    options.add_argument("--disable-webgl")
+
+    chrome = os.path.abspath('driver/chromium/chrome')
+    if os.path.isfile(chrome):
+        options.binary_location = chrome
+    # start chrome driver
+    driver = webdriver.Chrome(
+        executable_path=os.path.abspath('driver/chromedriver'),
+        options=options)
+
+    return driver
+
+
+def get_element(driver, ID):
+    timeout = 10
+    try:
+        element = WebDriverWait(driver, timeout).until(
+            ec.element_to_be_clickable((By.ID, ID)))
+
+    except TimeoutException:
+        driver.quit()
+
+    return element
+
+
+def format_date(date):
+    time = datetime \
+        .strptime(str(date), '%d/%m/%Y') \
+        .strftime('%y-%m-%d')
+
+    return time
+
+
+def save_data(data):
+    file = C.FILE
+    # if not os.path.isfile(file):
+    try:
+        file = open(file, "w")
+        json.dump(data, file)
+        file.close()
+
+    except OSError as eos:
+        print('OSError:', eos)
+
+    except IOError as eio:
+        print("IOError:", eio)
+
+
+def read_data():
+    try:
+        file = open(C.FILE, "r")
+        json_data = json.load(file)
+        file.close()
+
+    except IOError as eio:
+        print("IOError:", eio)
+
+    return json_data
+
+
+def print_json(json_data):
+    print(json.dumps(json_data, indent=4, ensure_ascii=False))
+
+
+class api:
+    API = 'https://fohm.maps.arcgis.com/apps/opsdashboard/' \
+        'index.html#/68d4537bf2714e63b646c37f152f1392'
+
+
+class C:
+    FDATA = "\nFETCHING DATA...\n"
+    NEXT_ID = 'ember249'
+    NDAYS_ID = 'ember245'
+    TABLE_ID = 'ember252'
+    FILE = 'data.txt'
+
+
+if __name__ == "__main__":
+    main()
